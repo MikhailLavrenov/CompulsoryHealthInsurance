2d74d662069a7040a1437ef7f313bc822476cd13
ЛавреновМВ <@PC-05>
2019-05-30 11:42:39 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DBConnectionString")
+        { }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
