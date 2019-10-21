cf37b23c1137bdf163f197bc3a8d2df166c63423
MikhailLavrenov <mvlavrenov@mail.ru>
2019-10-21 23:05:42 +1000

    В разработке сервис поиска паицента в плане веб портала диспансеризации

--- CHI.Modules.MedicalExaminations/Services/WebServer.cs
+++ CHI.Modules.MedicalExaminations/Services/WebServer.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 
 namespace CHI.Modules.MedicalExaminations.Services
 {
@@ -11,6 +12,7 @@ namespace CHI.Modules.MedicalExaminations.Services
     {
         #region Поля        
         private HttpClient client;
+        private int drawCounter;
         #endregion
 
         #region Свойства
@@ -25,6 +27,8 @@ namespace CHI.Modules.MedicalExaminations.Services
         public WebServer(string URL, string proxyAddress, int proxyPort)
         {
             Authorized = false;
+            drawCounter = 0;
+
             var clientHandler = new HttpClientHandler();
             clientHandler.CookieContainer = new CookieContainer();
 
@@ -59,49 +63,80 @@ namespace CHI.Modules.MedicalExaminations.Services
         }
         public bool TryFindPatientInPlan(string insuranceNumber, ExaminationType examinationType, int year)
         {
-            var requestValues = new Dictionary<string, string>
+            var builder = new UriBuilder(@"/disp/GetDispData");
+            builder.Port = -1;
+
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            query["Filter.Year"] = GetYearId(year);
+            query["Filter.Smo"] = "";
+            query["Filter.PolisNum"] = insuranceNumber;
+            query["Filter.PersonId"] = "";
+            query["Filter.Surname"] = "";
+            query["Filter.Firstname"] = "";
+            query["Filter.Secname"] = "";
+            query["Filter.Region"] = "";
+            query["Filter.AnotherAttachment"] = "false";
+            query["Filter.Stage"] = "";
+            query["Filter.StageEmpty"] = "false";
+            query["Filter.StageFrom"] = "";
+            query["Filter.StageTo"] = "";
+            query["Filter.Result1Id"] = "";
+            query["Filter.Dest1Id"] = "";
+            query["Filter.Result2Id"] = "";
+            query["Filter.Dest2Id"] = "";
+            query["Filter.BillsDispStage"] = "";
+            query["Filter.BillsDispStageEmpty"] = "false";
+            query["Filter.BillsFrom"] = "";
+            query["Filter.BillsTo"] = "";
+            query["Filter.DispType"] = ((int)examinationType).ToString();
+
+            builder.Query = query.ToString();
+            string urn = builder.ToString();
+
+            var requestBodyValues = new Dictionary<string, string>
             {
-                { "Filter.Year",        GetYearId(year)                     },
-                { "Filter.PolisNum",    insuranceNumber                     },
-                { "Filter.DispType",    ((int)examinationType).ToString()   }
+                { "draw",   drawCounter++.ToString()    },
+                { "start",  "0"                         },
+                { "length", "25"                        },
             };
 
-            var isRequestSuccessful = TryPostReqest(@"/disp/GetDispData", requestValues, out var responseText);
+            var isRequestSuccessful = TryPostReqest(urn, requestBodyValues, out var responseText);
 
             return true;
 
-            // Query string parameters
-            var queryString = new Dictionary<string, string>() { { "foo", "bar" } };
+            //// Query string parameters
+            //var queryString = new Dictionary<string, string>() { { "foo", "bar" } };
 
-            // Create json for body
-            var content = new JObject(json);
+            //// Create json for body
+            //var content = new JObject(json);
 
-            // Create HttpClient
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.baseaddress.com/");
+            //// Create HttpClient
+            //var client = new HttpClient();
+            //client.BaseAddress = new Uri("https://api.baseaddress.com/");
 
-            // This is the missing piece
-            var requestUri = QueryHelpers.AddQueryString("something", queryString);
+            //// This is the missing piece
+            //var requestUri = QueryHelpers.AddQueryString("something", queryString);
 
-            var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
-            // Setup header(s)
-            request.Headers.Add("Accept", "application/json");
-            // Add body content
-            request.Content = new StringContent(
-                content.ToString(),
-                Encoding.UTF8,
-                "application/json"
-            );
+            //var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            //// Setup header(s)
+            //request.Headers.Add("Accept", "application/json");
+            //// Add body content
+            //request.Content = new StringContent(
+            //    content.ToString(),
+            //    Encoding.UTF8,
+            //    "application/json"
+            //);
 
-            // Send the request
-            client.SendAsync(request);
+            //// Send the request
+            //client.SendAsync(request);
         }
         private static string GetYearId(int year) => (year - 2017).ToString();
-        private bool TryPostReqest(string URN, Dictionary<string, string> requestValues, out string responseText)
+        private bool TryPostReqest(string URN, Dictionary<string, string> requestBodyValues, out string responseText)
         {
             try
             {
-                var content = new FormUrlEncodedContent(requestValues);
+                var content = new FormUrlEncodedContent(requestBodyValues);
 
                 var response = client.PostAsync(URN, content).GetAwaiter().GetResult();
 
