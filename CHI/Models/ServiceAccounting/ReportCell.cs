1c2028aa5a574fc74079a7aefab87d3289eaf864
MikhailLavrenov <mvlavrenov@mail.ru>
2020-03-18 21:42:24 +1000

    Разработка отчета по объемам

---
+++ CHI/Models/ServiceAccounting/ReportCell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CHI.Models.ServiceAccounting
+{
+    public class ReportCell
+    {
+        public Indicator Indicator { get; set; }
+        public double Value { get; set; }
+    }
+}
