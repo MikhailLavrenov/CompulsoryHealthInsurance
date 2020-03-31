5134d9c2a9781e0e7d1c175e826d20db3c499742
MikhailLavrenov <mvlavrenov@mail.ru>
2020-03-31 22:54:54 +1000

    Разработка отчета по объемам

--- CHI/Models/ServiceAccounting/Report/Report.cs
+++ CHI/Models/ServiceAccounting/Report/Report.cs
@@ -8,10 +8,14 @@ namespace CHI.Models.ServiceAccounting
     {
         HeaderGroup rootRowHeader;
         HeaderGroup rootColumnHeader;
+        int rowsCount;
+        int columnsCount;
+        int maxPriority;
 
         public List<HeaderItem> RowHeaders { get; private set; }
         public List<HeaderItem> ColumnHeaders { get; private set; }
         public ValueItem[,] Values { get; private set; }
+        public List<ValueItem> ValuesList { get; private set; }
 
         public Report(Department rootDepartment, Component rootComponent)
         {
@@ -65,15 +69,73 @@ namespace CHI.Models.ServiceAccounting
 
             Values = new ValueItem[parameters.Count, indicators.Count];
 
+            rowsCount = parameters.Count;
+            columnsCount = indicators.Count;
+            maxPriority = 0;
+
             for (int row = 0; row < parameters.Count; row++)
                 for (int col = 0; col < indicators.Count; col++)
+                {
                     Values[row, col] = new ValueItem(row, col, parameters[row], indicators[col]);
+
+                    if (Values[row, col].Priority > maxPriority)
+                        maxPriority = Values[row, col].Priority;
+                }
+
+            ValuesList = Values.Cast<ValueItem>().ToList();
         }
 
         public void Build(List<Case> cases)
         {
+            var employeesFactDataSource = new Dictionary<Employee, List<Case>>();
+
+            Enumerable.Range(0, Values.GetLength(0))
+                .Select(x => Values[x, 0].Parameter)
+                .Where(x => x.Kind == ParameterKind.EmployeeFact)
+                .Select(x => x.Employee)
+                .ToList()
+                .ForEach(x => employeesFactDataSource.Add(x, cases.Where(y => y.Employee == x).ToList()));
 
 
+            for (int priority = maxPriority; priority <= 0; priority--)
+                foreach (var valueItem in ValuesList.Where(x => x.Priority == priority))
+                {
+                    var parameter = valueItem.Parameter;
+                    var indicator= valueItem.Indicator;
+                    var component = valueItem.Indicator.Component;
+
+
+                    if (valueItem.Parameter.Kind == ParameterKind.EmployeeFact)
+                    {
+                        if (valueItem.Indicator.Component.CaseFilters.First().Kind == CaseFilterKind.Total)
+                        {
+
+                        }
+                        else
+                        {
+
+
+
+
+
+                        }
+
+
+
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+                }
 
 
 
