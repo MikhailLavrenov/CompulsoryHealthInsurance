ae2246809c1829f801df2f392144a56b57f880cf
ЛавреновМВ <@PC-05>
2020-04-10 17:01:37 +1000

    Разработка отчета по учету услуг

--- CHI/ViewModels/SpecialtiesViewModel.cs
+++ CHI/ViewModels/SpecialtiesViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using CHI.Infrastructure;
 using CHI.Models.ServiceAccounting;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using Prism.Regions;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace CHI.ViewModels
 {
@@ -10,18 +16,105 @@ namespace CHI.ViewModels
     {
         ServiceAccountingDBContext dbContext;
         ObservableCollection<Specialty> specialties;
+        IMainRegionService mainRegionService;
+        IFileDialogService fileDialogService;
 
         public bool KeepAlive { get => false; }
         public ObservableCollection<Specialty> Specialties { get => specialties; set => SetProperty(ref specialties, value); }
 
-        public SpecialtiesViewModel(IMainRegionService mainRegionService)
+        public DelegateCommandAsync LoadCommand { get; }
+        public DelegateCommandAsync SaveExampleCommand { get; }
+
+
+        public SpecialtiesViewModel(IMainRegionService mainRegionService, IFileDialogService fileDialogService)
         {
+            this.mainRegionService = mainRegionService;
+            this.fileDialogService = fileDialogService;
+
             mainRegionService.Header = "Специальности";
 
             dbContext = new ServiceAccountingDBContext();
             dbContext.Specialties.Load();
             Specialties = dbContext.Specialties.Local.ToObservableCollection();
+
+            LoadCommand = new DelegateCommandAsync(LoadExecute);
+            SaveExampleCommand = new DelegateCommandAsync(SaveExampleExecute);
         }
+
+        private void LoadExecute()
+        {
+            mainRegionService.SetBusyStatus("Выбор файла");
+
+            fileDialogService.DialogType = FileDialogType.Open;
+            fileDialogService.Filter = "Excel files (*.xlsx)|*.xlsx";
+
+            if (fileDialogService.ShowDialog() != true)
+            {
+                mainRegionService.SetCompleteStatus("Отменено");
+                return;
+            }
+
+            mainRegionService.SetBusyStatus("Загрузка файла");
+
+            using var excel = new ExcelPackage(new FileInfo(fileDialogService.FileName));
+            var sheet = excel.Workbook.Worksheets.First();
+
+            for (int i = 2; i <= sheet.Dimension.Rows; i++)
+            {
+                var loadedFomsId = int.Parse(sheet.Cells[i, 1].Value.ToString(), CultureInfo.InvariantCulture);
+                var loadedName = sheet.Cells[i, 2].Value.ToString();
+
+                var specialty = Specialties.FirstOrDefault(x => x.FomsId == loadedFomsId);
+
+                if (specialty != null)
+                    specialty.Name = loadedName;
+            }
+
+            Specialties = dbContext.Specialties.Local.ToObservableCollection();
+
+            mainRegionService.SetCompleteStatus("Успешно загружено");
+        }
+
+        private void SaveExampleExecute()
+        {
+            mainRegionService.SetBusyStatus("Выбор пути");
+
+            fileDialogService.DialogType = FileDialogType.Save;
+            fileDialogService.FileName = "Пример для загрузки специальностей";
+            fileDialogService.Filter = "Excel files (*.xslx)|*.xlsx";
+
+            if (fileDialogService.ShowDialog() != true)
+                return;
+
+            var saveExampleFilePath = fileDialogService.FileName;
+
+            mainRegionService.SetBusyStatus("Сохранение файла");
+
+            using var excel = new ExcelPackage();
+
+            var sheet = excel.Workbook.Worksheets.Add("Лист1");
+
+            sheet.Cells.LoadFromArrays(new string[][] { new[] { "Код ФОМС", "Специальность" } });
+
+            var collection = new List<Tuple<int, string>> {
+
+                new Tuple<int, string> ( 11,     "Терапевт"    ),
+                new Tuple<int, string> ( 23,     "Хирург"      ),
+                new Tuple<int, string> ( 16,     "Педиатр"     ),
+                new Tuple<int, string> ( 57,     "Офтальмолог" ),
+                new Tuple<int, string> ( 3,      "Гинеколог"   ),
+                new Tuple<int, string> ( 55,     "Фельдшер"    ),
+            };
+
+            sheet.Cells[2, 1].LoadFromCollection(collection);
+            sheet.Cells.AutoFitColumns();
+            sheet.SelectedRange[1, 1, 1, 2].Style.Font.Bold = true;
+
+            excel.SaveAs(new FileInfo(fileDialogService.FileName));
+
+            mainRegionService.SetCompleteStatus($"Файл сохранен: {saveExampleFilePath}");
+        }
+
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
