c8656602faf45cb6b6ac562bcde20d4769ca124f
MikhailLavrenov <mvlavrenov@mail.ru>
2020-05-18 20:42:32 +1000

    Разработка учета услуг

--- CHI/Migrations/20200518102925_UserPermissionsMigration.Designer.cs
+++ CHI/Migrations/20200518102925_UserPermissionsMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CHI.Models.ServiceAccounting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CHI.Migrations
 {
     [DbContext(typeof(ServiceAccountingDBContext))]
-    partial class ServiceAccountingDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200518102925_UserPermissionsMigration")]
+    partial class UserPermissionsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CHI/Migrations/20200518102925_UserPermissionsMigration.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CHI.Migrations
+{
+    public partial class UserPermissionsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AttachedPatientsPermision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MedicalExaminationsPermision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ReferencesPerimision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RegistersPermision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SettingsPermision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UsersPerimision",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachedPatientsPermision",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MedicalExaminationsPermision",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ReferencesPerimision",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RegistersPermision",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SettingsPermision",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UsersPerimision",
+                table: "Users");
+        }
+    }
+}
