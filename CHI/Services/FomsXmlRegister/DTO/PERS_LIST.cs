9880170af7a3936b0d7aaec332a1027bdffd71ec
MikhailLavrenov <mvlavrenov@mail.ru>
2021-08-12 23:12:53 +1000

    Рефакторинг

--- CHI/Services/FomsXmlRegister/DTO/PERS_LIST.cs
+++ CHI/Services/FomsXmlRegister/DTO/PERS_LIST.cs
@@ -7,12 +7,18 @@ namespace CHI.Services
     /// Представляет информацию о пациентах реестра-счетов.
     /// </summary>
     [XmlRoot(ElementName = "PERS_LIST")]
-    public class PERS_LIST: REGISTER_LIST
+    public class PERS_LIST
     {
         /// <summary>
         /// Список сведений о пациентах
         /// </summary>
         [XmlElement(ElementName = "PERS")]
         public List<PERS> PERS { get; set; }
+
+        /// <summary>
+        /// Заголовок файла
+        /// </summary>
+        [XmlElement(ElementName = "ZGLV")]
+        public ZGLV ZGLV { get; set; }
     }
 }
