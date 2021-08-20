db52eb15aefa1bebe68afc48e6354225de37fcd2
MikhailLavrenov <mvlavrenov@mail.ru>
2021-08-20 20:24:44 +1000

    Рефакторинг

--- CHI/ViewModels/ExaminationsViewModel.cs
+++ CHI/ViewModels/ExaminationsViewModel.cs
@@ -18,7 +18,7 @@ namespace CHI.ViewModels
     class ExaminationsViewModel : DomainObject, IRegionMemberLifetime
     {
         Settings settings;
-        List<Tuple<PatientExaminations, bool, string>> result;
+        List<(PatientExaminations PatientExaminations, bool IsLoaded, string Error)> result;
         bool showErrors;
         readonly IFileDialogService fileDialogService;
 
@@ -27,7 +27,7 @@ namespace CHI.ViewModels
         public ILicenseManager LicenseManager { get; set; }
         public bool KeepAlive { get => false; }
         public bool ShowErrors { get => showErrors; set => SetProperty(ref showErrors, value); }
-        public List<Tuple<PatientExaminations, bool, string>> Result { get => result; set => SetProperty(ref result, value); }
+        public List<(PatientExaminations PatientExaminations, bool IsLoaded, string Error)> Result { get => result; set => SetProperty(ref result, value); }
         public Settings Settings { get => settings; set => SetProperty(ref settings, value); }
         public DelegateCommandAsync ExportExaminationsCommand { get; }
 
@@ -38,7 +38,7 @@ namespace CHI.ViewModels
             MainRegionService = mainRegionService;
             LicenseManager = licenseManager;
 
-            Result = new List<Tuple<PatientExaminations, bool, string>>();
+            Result = new List<(PatientExaminations, bool, string)>();
 
             ShowErrors = false;
             Settings = Settings.Instance;
@@ -118,7 +118,7 @@ namespace CHI.ViewModels
             Settings.ExaminationsFileDirectory = Path.GetDirectoryName(fileDialogService.FileNames.FirstOrDefault());
 
             MainRegionService.ShowProgressBar("Чтение файлов.");
-            
+
             var registers = new MedExamsBillsRegisterService();
             var fileFilter = $"{Settings.PatientFileNames},{Settings.ExaminationFileNames}".Split(',', StringSplitOptions.TrimEntries);
             registers.XmlFileNameStartsWithFilter = fileFilter.ToList();
@@ -139,10 +139,11 @@ namespace CHI.ViewModels
 
             MainRegionService.ShowProgressBar($"Загрузка осмотров. Всего пациентов: {patientsExaminations.Count}.");
 
-            Result = AddExaminationsParallel(patientsExaminations)
-                .OrderBy(x => x.Item2)
-                .ThenBy(x => x.Item1.Kind)
-                .ThenBy(x => x.Item1.Year)
+            Result = await AddExaminationsParallel(patientsExaminations);
+
+            Result.OrderBy(x => x.IsLoaded)                
+                .ThenBy(x => x.PatientExaminations.Kind)
+                .ThenBy(x => x.PatientExaminations.Year)
                 .ToList();
 
             if (Result?.Count > 0)
@@ -157,109 +158,200 @@ namespace CHI.ViewModels
         /// </summary>
         /// <param name="patientsExaminations">Список профилактических осмотров пациентов.</param>
         /// <returns>Список кортежей состоящий из PatientExaminations, флага успешной загрузки (true-успешно, false-иначе), строки с сообщением об ошибке.</returns>
-        private List<Tuple<PatientExaminations, bool, string>> AddExaminationsParallel(List<PatientExaminations> patientsExaminations)
+        async Task<List<(PatientExaminations, bool, string)>> AddExaminationsParallel(List<PatientExaminations> patientsExaminations)
         {
-            var threadsLimit = patientsExaminations.Count > Settings.ExaminationsThreadsLimit ? Settings.ExaminationsThreadsLimit : patientsExaminations.Count;
-            var result = new ConcurrentBag<Tuple<PatientExaminations, bool, string>>();
-            var tasks = new Task<ExaminationService>[threadsLimit];
-            var counter = 0;
+            var loadedExaminations = new ConcurrentBag<(PatientExaminations PatientExaminations, bool IsLoaded, string Error)>();
+            var tasks = new Task[Math.Min(Settings.ExaminationsThreadsLimit, patientsExaminations.Count)];
+            var patientsExaminationsStack = new ConcurrentStack<PatientExaminations>(patientsExaminations);
+
             //задержка потока перед обращением к веб-серверу, увеличивается при росте неудачных попыток, и уменьшается при росте удачных
             var sleepTime = 0;
 
-            for (int i = 0; i < threadsLimit; i++)
-                tasks[i] = Task.Run(() => (ExaminationService)null);
-
-            for (int i = 0; i < patientsExaminations.Count; i++)
-            {
-                var patientExaminations = patientsExaminations[i];
-                var index = Task.WaitAny(tasks);
-                tasks[index] = tasks[index].ContinueWith(async (task) =>
-                {
-                    var service = await task;
-                    string error = string.Empty;
-                    bool isSuccessful = true;
-
-                    //3 попытки на загрузку осмотра, т.к. иногда веб-сервер обрывает сессии
-                    for (int j = 1; j < 4; j++)
-                    {
-                        if (j != 1)
-                            Interlocked.Add(ref sleepTime, 5000);
-
-                        Thread.Sleep(sleepTime);
-
-                        try
-                        {
-                            if (service == null)
-                            {
-                                service = new ExaminationService(Settings.ExaminationsAddress, Settings.UseProxy, Settings.ProxyAddress, Settings.ProxyPort);
-                                await service.AuthorizeAsync(settings.ExaminationsCredentials.First());
-                            }
-
-                            await service.AddPatientExaminationsAsync(patientExaminations);
-                            error = string.Empty;
-                            isSuccessful = true;
-
-                            if (sleepTime != 0)
-                            {
-                                //универсальный InterLocked-паттерн, потокобезопасно уменьшает sleepRate если он положительный
-                                int initial, desired;
-
-                                do
-                                {
-                                    initial = sleepTime;
-                                    desired = initial;
-                                    if (desired >= 1000)
-                                        desired -= 1000;
-                                    else if (desired > 0)
-                                        desired = 0;
-                                }
-                                while (initial != Interlocked.CompareExchange(ref sleepTime, desired, initial));
-                            }
-
-                            break;
-                        }
-                        catch (HttpRequestException ex)
-                        {
-                            error = ex.Message;
-                            isSuccessful = false;
-                            service = null;
-                        }
-                        catch (InvalidOperationException ex)
-                        {
-                            error = ex.Message;
-                            isSuccessful = false;
-                        }
-                        catch (WebServiceOperationException ex)
-                        {
-                            error = ex.Message;
-                            isSuccessful = false;
-                            break;
-                        }
-                    }
-
-                    result.Add(new Tuple<PatientExaminations, bool, string>(patientExaminations, isSuccessful, error));
-                    Interlocked.Increment(ref counter);
-                    MainRegionService.ShowProgressBar($"Загрузка осмотров. Обработано пациентов: {counter} из {patientsExaminations.Count}.");
-
-                    return service;
-                });
-            }
-
             for (int i = 0; i < tasks.Length; i++)
             {
-                var index = Task.WaitAny(tasks);
-
-                tasks[index] = tasks[index].ContinueWith(async (task) =>
+                tasks[i] = Task.Factory.StartNew(async () =>
                 {
-                    var service =await task;
-                    await service?.LogoutAsync();
-                    return service;
+                    ExaminationService service = null;
+
+                    while (patientsExaminationsStack.TryPop(out var patientExaminations))
+                    {
+                        string error = string.Empty;
+                        bool isSuccessful = true;
+
+                        //3 попытки на загрузку осмотра, т.к. иногда веб-сервер обрывает сессии
+                        for (int j = 1; j < 4; j++)
+                        {
+                            if (j != 1)
+                                Interlocked.Add(ref sleepTime, 5000);
+
+                            Thread.Sleep(sleepTime);
+
+                            try
+                            {
+                                if (service == null)
+                                {
+                                    service = new ExaminationService(Settings.ExaminationsAddress, Settings.UseProxy, Settings.ProxyAddress, Settings.ProxyPort);
+                                    await service.AuthorizeAsync(settings.ExaminationsCredentials.First());
+                                }
+
+                                await service.AddPatientExaminationsAsync(patientExaminations);
+                                error = string.Empty;
+                                isSuccessful = true;
+
+                                if (sleepTime != 0)
+                                {
+                                    //универсальный InterLocked-паттерн, потокобезопасно уменьшает sleepRate если он положительный
+                                    int initial, desired;
+
+                                    do
+                                    {
+                                        initial = sleepTime;
+                                        desired = initial;
+                                        if (desired >= 1000)
+                                            desired -= 1000;
+                                        else if (desired > 0)
+                                            desired = 0;
+                                    }
+                                    while (initial != Interlocked.CompareExchange(ref sleepTime, desired, initial));
+                                }
+
+                                break;
+                            }
+                            catch (HttpRequestException ex)
+                            {
+                                error = ex.Message;
+                                isSuccessful = false;
+                                service = null;
+                            }
+                            //??????????????????????????
+                            catch (InvalidOperationException ex)
+                            {
+                                error = ex.Message;
+                                isSuccessful = false;
+                            }
+                            catch (WebServiceOperationException ex)
+                            {
+                                error = ex.Message;
+                                isSuccessful = false;
+                                break;
+                            }
+                        }
+
+                        loadedExaminations.Add((patientExaminations, isSuccessful, error));
+
+                        MainRegionService.ShowProgressBar($"Загрузка осмотров. Обработано пациентов: {loadedExaminations.Count} из {patientsExaminations.Count}.");
+                    }
                 });
             }
 
-            Task.WaitAll(tasks);
+            await Task.WhenAll(tasks);
 
-            return result.ToList();
+            return loadedExaminations.ToList();
         }
+
+        //private List<Tuple<PatientExaminations, bool, string>> AddExaminationsParallel(List<PatientExaminations> patientsExaminations)
+        //{
+        //    var threadsLimit = patientsExaminations.Count > Settings.ExaminationsThreadsLimit ? Settings.ExaminationsThreadsLimit : patientsExaminations.Count;
+        //    var result = new ConcurrentBag<Tuple<PatientExaminations, bool, string>>();
+        //    var tasks = new Task<ExaminationService>[threadsLimit];
+        //    var counter = 0;
+        //    //задержка потока перед обращением к веб-серверу, увеличивается при росте неудачных попыток, и уменьшается при росте удачных
+        //    var sleepTime = 0;
+
+        //    for (int i = 0; i < threadsLimit; i++)
+        //        tasks[i] = Task.Run(() => (ExaminationService)null);
+
+        //    for (int i = 0; i < patientsExaminations.Count; i++)
+        //    {
+        //        var patientExaminations = patientsExaminations[i];
+        //        var index = Task.WaitAny(tasks);
+        //        tasks[index] = tasks[index].ContinueWith(async (task) =>
+        //        {
+        //            var service = await task;
+        //            string error = string.Empty;
+        //            bool isSuccessful = true;
+
+        //            //3 попытки на загрузку осмотра, т.к. иногда веб-сервер обрывает сессии
+        //            for (int j = 1; j < 4; j++)
+        //            {
+        //                if (j != 1)
+        //                    Interlocked.Add(ref sleepTime, 5000);
+
+        //                Thread.Sleep(sleepTime);
+
+        //                try
+        //                {
+        //                    if (service == null)
+        //                    {
+        //                        service = new ExaminationService(Settings.ExaminationsAddress, Settings.UseProxy, Settings.ProxyAddress, Settings.ProxyPort);
+        //                        await service.AuthorizeAsync(settings.ExaminationsCredentials.First());
+        //                    }
+
+        //                    await service.AddPatientExaminationsAsync(patientExaminations);
+        //                    error = string.Empty;
+        //                    isSuccessful = true;
+
+        //                    if (sleepTime != 0)
+        //                    {
+        //                        //универсальный InterLocked-паттерн, потокобезопасно уменьшает sleepRate если он положительный
+        //                        int initial, desired;
+
+        //                        do
+        //                        {
+        //                            initial = sleepTime;
+        //                            desired = initial;
+        //                            if (desired >= 1000)
+        //                                desired -= 1000;
+        //                            else if (desired > 0)
+        //                                desired = 0;
+        //                        }
+        //                        while (initial != Interlocked.CompareExchange(ref sleepTime, desired, initial));
+        //                    }
+
+        //                    break;
+        //                }
+        //                catch (HttpRequestException ex)
+        //                {
+        //                    error = ex.Message;
+        //                    isSuccessful = false;
+        //                    service = null;
+        //                }
+        //                catch (InvalidOperationException ex)
+        //                {
+        //                    error = ex.Message;
+        //                    isSuccessful = false;
+        //                }
+        //                catch (WebServiceOperationException ex)
+        //                {
+        //                    error = ex.Message;
+        //                    isSuccessful = false;
+        //                    break;
+        //                }
+        //            }
+
+        //            result.Add(new Tuple<PatientExaminations, bool, string>(patientExaminations, isSuccessful, error));
+        //            Interlocked.Increment(ref counter);
+        //            MainRegionService.ShowProgressBar($"Загрузка осмотров. Обработано пациентов: {counter} из {patientsExaminations.Count}.");
+
+        //            return service;
+        //        });
+        //    }
+
+        //    for (int i = 0; i < tasks.Length; i++)
+        //    {
+        //        var index = Task.WaitAny(tasks);
+
+        //        tasks[index] = tasks[index].ContinueWith(async (task) =>
+        //        {
+        //            var service = await task;
+        //            await service?.LogoutAsync();
+        //            return service;
+        //        });
+        //    }
+
+        //    Task.WaitAll(tasks);
+
+        //    return result.ToList();
+        //}
     }
 }
