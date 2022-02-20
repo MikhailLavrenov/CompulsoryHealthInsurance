a7ee10ba63592eb6dc4644912a4d57b5efcb4670
MikhailLavrenov <mvlavrenov@mail.ru>
2022-02-20 20:23:01 +1000

    Доделать стил кнопки

---
+++ CHI/Infrastructure/Controls/CustomButton.cs
@@ -0,0 +1,39 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CHI.Infrastructure
+{
+    public class CustomButton : Button
+    {
+        // свойство зависимостей
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+                        "Text",
+                        typeof(string),
+                        typeof(CustomButton),                                
+                        new FrameworkPropertyMetadata(                        
+                            string.Empty,                        
+                            FrameworkPropertyMetadataOptions.AffectsMeasure |                        
+                            FrameworkPropertyMetadataOptions.AffectsRender|                                     
+                            FrameworkPropertyMetadataOptions.Inherits
+                        ));
+
+        public static readonly DependencyProperty IconKindProperty = DependencyProperty.Register(
+                "IconKind",
+                typeof(PackIconKind),
+                typeof(CustomButton));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public PackIconKind IconKind
+        {
+            get { return (PackIconKind)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+    }
+}
