ddf32fe466420a2e1962e3c157affb16f01040cc
ЛавреновМВ <@PC-05>
2022-02-24 17:14:33 +1000

    Исправлены баги

--- CHI/ViewModels/AttachedPatientsFileSettingsViewModel.cs
+++ CHI/ViewModels/AttachedPatientsFileSettingsViewModel.cs
@@ -3,17 +3,23 @@ using CHI.Models;
 using CHI.Settings;
 using Prism.Commands;
 using Prism.Regions;
+using System.Linq;
 
 namespace CHI.ViewModels
 {
     public class AttachedPatientsFileSettingsViewModel : DomainObject, IRegionMemberLifetime
     {
+        ColumnProperty currentColumnProperty;
+
         public IMainRegionService MainRegionService { get; set; }
         public bool KeepAlive { get => false; }
         public AttachedPatientsFileSettings Settings { get; set; }
+        public ColumnProperty CurrentColumnProperty { get => currentColumnProperty; set => SetProperty(ref currentColumnProperty, value); }
+
+
         public DelegateCommand SetDefaultCommand { get; }
-        public DelegateCommand<ColumnProperty> MoveUpCommand { get; }
-        public DelegateCommand<ColumnProperty> MoveDownCommand { get; }
+        public DelegateCommand MoveUpCommand { get; }
+        public DelegateCommand MoveDownCommand { get; }
 
 
         public AttachedPatientsFileSettingsViewModel(AppSettings settings, IMainRegionService mainRegionService)
@@ -24,15 +30,56 @@ namespace CHI.ViewModels
             MainRegionService.Header = "Форматирование файла прикрепленных пациентов";
 
             SetDefaultCommand = new DelegateCommand(SetDefaultExecute);
-            MoveUpCommand = new DelegateCommand<ColumnProperty>(x => Settings.MoveUpColumnProperty(x));
-            MoveDownCommand = new DelegateCommand<ColumnProperty>(x => Settings.MoveDownColumnProperty(x));
+            MoveUpCommand = new DelegateCommand(MoveUpExecute, MoveUpCanExecute)                
+                .ObservesProperty(() => CurrentColumnProperty)
+                .ObservesProperty(() => Settings.ApplyFormat);
+            MoveDownCommand = new DelegateCommand(MoveDownExecute, MoveDownCanExecute)
+                .ObservesProperty(() => CurrentColumnProperty)
+                .ObservesProperty(() => Settings.ApplyFormat);
         }
 
-
-        private void SetDefaultExecute()
+        void SetDefaultExecute()
         {
             Settings.SetDefault();
             MainRegionService.HideProgressBar("Настройки установлены по умолчанию.");
+        }
+
+        void MoveUpExecute()
+        {
+            Settings.MoveUpColumnProperty(CurrentColumnProperty);
+
+            MoveDownCommand.RaiseCanExecuteChanged();
+            MoveUpCommand.RaiseCanExecuteChanged();
+        }
+
+        void MoveDownExecute()
+        {
+            Settings.MoveDownColumnProperty(CurrentColumnProperty);
+
+            MoveDownCommand.RaiseCanExecuteChanged();
+            MoveUpCommand.RaiseCanExecuteChanged();
+        }
+
+        bool MoveUpCanExecute()
+        {
+            if (CurrentColumnProperty == null)
+                return false;
+
+            if ((Settings.ColumnProperties?.Any() ?? false) == false || Settings.ColumnProperties.First() == CurrentColumnProperty)
+                return false;
+
+            return Settings.ApplyFormat;
+        }
+
+        bool MoveDownCanExecute()
+        {
+            if (CurrentColumnProperty == null)
+                return false;
+
+            if ((Settings.ColumnProperties?.Any() ?? false) == false || Settings.ColumnProperties.Last() == CurrentColumnProperty)
+                return false;
+
+            return Settings.ApplyFormat;
         }
     }
 }
